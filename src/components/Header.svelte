676b78dfee724047fa8ca487a0cacccc3e073f04
Hardik <dev.hardik200@gmail.com>
2021-07-29 13:09:30 +0530

    Header Changed

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,15 +1,14 @@
 <script>
-  const svelteLogo = '/images/svelte-logo.svg'
-  const tmdbLogo = '/images/tmdb_logo.svg'
+  const svelteLogo = "/images/svelte-logo.svg";
+  const tmdbLogo = "/images/tmdb_logo.svg";
 </script>
+
 <header class="wrapper">
   <div class="content">
-    <a href="/" aria-label="Go Home">
-    <img class="svelte-logo" src={svelteLogo} alt="Svelte Logo">
-    </a>
-    <img class="tmdb-logo" src={tmdbLogo} alt="TMDB Logo">
+    <h1>Svelte Movie</h1>
   </div>
 </header>
+
 <style>
   .wrapper {
     background: #1c1c1c;
@@ -19,22 +18,16 @@
   .content {
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: center;
+    text-align: center;
     max-width: 1280px;
-    min-height: 120px;
-    padding: 20px 0px;
+    height: 8vh;
     margin: 0 auto;
     box-sizing: border-box;
   }
 
-  .svelte-logo {
-    width: 250px;
-    margin-top: 20px;
-  }
-
-  .tmdb-logo {
-    width: 122px;
-    margin-top: 25px;
+  h1 {
+    color: white;
   }
 
   @media screen and (max-width: 500px) {
@@ -42,16 +35,8 @@
       max-width: 1280px;
       min-height: 0px;
     }
-
-    .svelte-logo {
-      width: 150px;
-      margin-top: 5px;
-    }
-
-    .tmdb-logo {
-      display: inline-block;
-      width: 80px;
-      margin-top: 0px;
+    h1 {
+      font-size: 25px;
     }
   }
 </style>
